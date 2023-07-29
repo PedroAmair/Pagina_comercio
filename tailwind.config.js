f8fa5027bac0afe50fc9b35d9ee682a5795d99e5
Pedro Amair <pedroamair@hotmail.com>
2023-07-29 11:11:05 -0400

    cambios al config de tailwind y resubido el build

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
-    "./node_modules/flowbite/**/*.js",
+    //"./node_modules/flowbite/**/*.js",
     "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php"
   ],
   theme: {

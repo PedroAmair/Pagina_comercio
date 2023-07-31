b670720f20705239fb11eb0beb4942bbc8134015
Pedro Amair <pedroamair@hotmail.com>
2023-07-31 10:18:29 -0400

    Eliminado el div de asus de la sección de marcas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -247,12 +247,6 @@
 
             <div class="transition hover:scale-125">
                 <a href="#">
-                    <img class="rounded-t-lg w-32 h-32 bg-gray-200 p-4" src="{{ asset('img/companiesLogos/asus.png')}}" alt="asus logo">
-                </a>
-            </div>
-
-            <div class="transition hover:scale-125">
-                <a href="#">
                     <img class="rounded-t-lg w-28 h-28 bg-gray-200 p-4" src="{{ asset('img/companiesLogos/msi.png')}}" alt="msi logo">
                 </a>
             </div>

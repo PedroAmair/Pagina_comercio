85c4d27193356d7558576ec4a08997e61b3b24cb
Pedro Amair <pedroamair@hotmail.com>
2024-01-29 19:03:37 -0400

    cambios en las dimensiones de los procesadores y productos en oferta del home

--- resources/views/components/products-list.blade.php
+++ resources/views/components/products-list.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="flex gap-4 overflow-hidden scroll-smooth" id="sliderOf">
         @foreach ($discounts as $discount)
-            <div class="min-w-[25%] md:min-w-[20%] lg:min-w-[17%] xl:min-w-12%">
+            <div class="min-w-[25%] md:min-w-[20%] lg:min-w-[17%] xl:min-w-[12%]">
                 <a href="{{route('searchs.show', $discount->id)}}">
                     <img loading="lazy" src="{{ asset('uploads').'/'.$discount->image }}" alt="{{ $discount->name }}">
                     <p class="text-center font-bold 2xl:text-2xl">{{ ucfirst(strtolower($discount->brand)).' '.$discount->name }}</p>

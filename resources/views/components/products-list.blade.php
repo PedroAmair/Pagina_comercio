70e0586b48d812071a649876be0b9f1e32c144f6
Pedro Amair <pedroamair@hotmail.com>
2024-01-30 09:48:13 -0400

    cambio en el tamaño de las fuentes de la cinta de descuentos

--- resources/views/components/products-list.blade.php
+++ resources/views/components/products-list.blade.php
@@ -4,7 +4,7 @@
             <div class="min-w-[25%] md:min-w-[20%] lg:min-w-[17%] xl:min-w-[12%]">
                 <a href="{{route('searchs.show', $discount->id)}}">
                     <img loading="lazy" src="{{ asset('uploads').'/'.$discount->image }}" alt="{{ $discount->name }}">
-                    <p class="text-center font-bold 2xl:text-2xl">{{ ucfirst(strtolower($discount->brand)).' '.$discount->name }}</p>
+                    <p class="text-center font-bold 2xl:text-xl">{{ ucfirst(strtolower($discount->brand)).' '.$discount->name }}</p>
                     <p class="text-center text-green-700 font-bold 2xl:text-2xl">{{ $discount->price}}$</p>
                 </a>
             </div>

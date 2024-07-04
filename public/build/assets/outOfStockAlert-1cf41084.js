79c60f4103ac5a89be2bbea5a7a412fedeac45e0
Pedro Amair <pedroamair@hotmail.com>
2024-07-04 19:40:10 -0400

    agregado archivo javascript a vite- contrucción para producción

---
+++ public/build/assets/outOfStockAlert-1cf41084.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){o()});function o(){n()}function n(){const t=document.querySelector("#outOfStock");t&&setTimeout(()=>{t.classList.add("hidden")},1e4)}

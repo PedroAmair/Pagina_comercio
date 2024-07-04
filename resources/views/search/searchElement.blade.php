4af3afbe411d6b307d5475c9ee0ee9daa163c982
Pedro Amair <pedroamair@hotmail.com>
2024-07-04 19:52:53 -0400

    eliminado botón de 'purchase now'. Cambios realizados a botón de 'add to cart'

--- resources/views/search/searchElement.blade.php
+++ resources/views/search/searchElement.blade.php
@@ -74,18 +74,13 @@
                         </span>
                     </p>
                 </div>
-                <div class="grid grid-cols-2 gap-4 mx-4">
-                    <input 
-                        type="submit"
-                        value="Purchase now"
-                        form="quantityForm"
-                        class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer
-                        uppercase font-bold w-full p-3 text-white rounded-lg my-5"
-                    />
-                    
-                    <button type="submit" form="quantityForm" class="bg-sky-800 hover:bg-sky-900 transition-colors cursor-pointer
+                <div class="grid grid-cols-1 gap-4 mx-4">
+                    <button type="submit" form="quantityForm" class="flex items-center gap-2 justify-center bg-sky-800 hover:bg-sky-900 transition-colors cursor-pointer
                                 uppercase font-bold w-full p-3 text-white rounded-lg my-5">
-                        add to cart
+                        add to cart 
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
                     </button>
                 </div>
             </div>
